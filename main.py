4d6d1735d6f711f25baacd963cfa21850d476bf5
Justin K -Linux <justinjoseph0007@gmail.com>
2020-04-05 17:21:06 -0400

    Deleted second files, now I just need to tune the network

--- main.py
+++ main.py
@@ -1,41 +1,71 @@
+from neural_net import NeuralNetwork
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from torch import optim
+import data
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-from neural_net import NeuralNetwork
-import data
 
 
-def spit_out_name_from_output(output):
-    max_val, _ = torch.max(output, 0)
-    index = _.item()
-    if (index == 0):  # the index number
-        return "malignant"
-    else:
-        return "benign"
+def test_trained_network(model, test_X, test_Y):
+    _input = torch.tensor(test_X, dtype=torch.float)
+    out = model(_input)
+    error = 0
+    tot = 0
+    mal_guessed_wrong = 0
+    ben_guessed_wrong = 0
+    for exp, ans in zip(out, test_Y):
+        tot += 1
+        max_val, _ = torch.max(exp, 0)
+        index = _.item()
+        if (index == ans):
+            continue
+        else:
+            error += 1
+            if (index == 0):
+                mal_guessed_wrong += 1
+            else:
+                ben_guessed_wrong += 1
 
-def test_trained_network(ANN):
-    print("Trained network being tested...\n")
-    error_count = 0
-
-    scaled_input = data.testing_input_mod
-    for i in range(len(data.testing_input_mod)):
-        _input = torch.tensor(scaled_input[i], dtype=torch.float)
-        exp_output = data.testing_output[i]
-        print("Expected output  : ", exp_output)
-        _output = ANN.predict(_input)
-        print("Predicted output : ", spit_out_name_from_output(_output), "\n")
-        if (spit_out_name_from_output(_output) != exp_output):
-            error_count += 1
-    print("Error count after testing", len(data.testing_input), "inputs: ",
-          error_count)
+    print("Error is {}".format(error))
+    print("Total is {}".format(tot))
+    print("Malignant guessed wrong is {}".format(mal_guessed_wrong))
+    print("Benign guessed wrong is {}".format(ben_guessed_wrong))
 
 
 def main():
-    scaled_input = data.training_input_mod
+    # load IRIS dataset
+    dataset = pd.read_csv('WDBC_modified.dat')
+
+    # transform species to numerics
+    dataset.loc[dataset.diagnosis == 'M', 'diagnosis'] = 0
+    dataset.loc[dataset.diagnosis == 'B', 'diagnosis'] = 1
+
+    train_X, test_X, train_Y, test_Y = train_test_split(
+        dataset[dataset.columns[2:12]].values,
+        dataset.diagnosis.values,
+        test_size=0.3,
+    )
+    train_Y = train_Y.astype(np.float32)
+    test_Y = train_Y.astype(np.float32)
+
+    sc = StandardScaler()
+    train_X = sc.fit_transform(train_X)
+    test_X = sc.fit_transform(test_X)
 
     # load data into tensor
-    _input = torch.tensor(scaled_input, dtype=torch.float)
-    _output = torch.tensor(data.training_expected_output, dtype=torch.float)
+    _input = torch.tensor(train_X, dtype=torch.float)
+    _output = torch.tensor(train_Y, dtype=torch.long)
+
+    # input,output,hidden layer size
+    model = NeuralNetwork(i=10, h1=6, h2=2, o=2)
+
+    # loss function and optimizer
+    lossFunction = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.002, momentum=0.9)
 
     # This section is for plotting ##############################
     gene_array = []
@@ -46,20 +76,27 @@ def main():
            title='Neural network, error loss after each generation')
     # This section is for plotting ##############################
 
-    # input,output,hidden layer size
-    ANN = NeuralNetwork(i=10, h1=6, h2=2, o=2)
-    # weight training
-    for i in range(15000):
-        # mean sum squared error
-        mean_error = torch.mean((_output - ANN(_input))**2).detach().item()
-        print("Generation: " + str(i) + " error: " + str(mean_error))
-        gene_array.append(i)
-        loss_array.append(mean_error)
-        ANN.train(_input, _output)
+    for epoch in range(15000):
+        # Forward Pass
+        output = model(_input)
+        # Loss at each oteration by comparing to target(label)
+        loss = lossFunction(output, _output)
 
-    torch.save(ANN, "algo1.weights")
-    # ANN = torch.load("algo1.weights")
-    test_trained_network(ANN)
+        # Backpropogating gradient of loss
+        optimizer.zero_grad()
+        loss.backward()
+
+        # Updating parameters(weights and bias)
+        optimizer.step()
+        _loss = loss.item()
+
+        gene_array.append(epoch)
+        loss_array.append(_loss)
+        print("Epoch {}, Training loss: {}".format(epoch, _loss / len(_input)))
+
+    torch.save(model, "algo1.weights")
+    # model = torch.load("algo1.weights")
+    test_trained_network(model, test_X, test_Y)
     ax.plot(gene_array, loss_array)
     plt.show()
 
